6b8a53033fd6f2304330ac3fb4bc99e7f1f43462
ChopperWriteCode <71073097+ChopperWriteCode@users.noreply.github.com>
2023-06-02 23:43:35 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -37,20 +37,37 @@ const TEMPLATE_CONFIG = [
   },
   {
     id: '0002',
-    title: '推送完成提醒',
+    title: '亲爱的小Q，早上好',
     desc: `
-      服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
+      desc: `
+      **{{date.DATA}}**
+      
+      下个休息日：{{holidaytts.DATA}}
       
       ---
       
-      共推送 {{need_post_num.DATA}} 人
+      城市：{{city.DATA}}
       
-      成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
+      天气：{{weather.DATA}}
       
-      成功用户: {{success_post_ids.DATA}}
+      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
       
-      失败用户: {{fail_post_ids.DATA}}
-    `
+      风向: {{wind_direction.DATA}}
+      
+      风级: {{wind_scale.DATA}}
+      
+      {{comprehensive_horoscope.DATA}}
+      
+      ---
+      
+      
+      {{birthday_message.DATA}}
+      
+      ---
+      
+      {{moment_copyrighting.DATA}}
+      
+      {{earthy_love_words.DATA}} {{note_ch.DATA}}
   },
 ]
 
