d4c03a54fdfb79f13d4cc3861d6638b5a79a7abd
ChopperWriteCode <71073097+ChopperWriteCode@users.noreply.github.com>
2023-06-02 23:51:53 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -35,40 +35,6 @@ const TEMPLATE_CONFIG = [
       {{earthy_love_words.DATA}} {{note_ch.DATA}}
     `
   },
-  {
-    id: '0002',
-    title: '亲爱的小Q，早上好',
-    desc: `
-      desc: `
-      **{{date.DATA}}**
-      
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{earthy_love_words.DATA}} {{note_ch.DATA}}
-  },
 ]
 
 module.exports = TEMPLATE_CONFIG
